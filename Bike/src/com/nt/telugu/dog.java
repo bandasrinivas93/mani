997d750e7f10c6b788c39240345fe56e042767d6
srinivas <bandasrinivas93@gmail.com>
2023-06-06 12:33:25 +0530

    monky mony

---
+++ Bike/src/com/nt/telugu/dog.java
@@ -0,0 +1,11 @@
+package com.nt.telugu;
+
+public class dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("commong to the dog");
+
+	}
+
+}
